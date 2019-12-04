fc1932b0f3c3c705338119601c6ff8fe7cbca6b8
robertpop99 <44144186+robertpop99@users.noreply.github.com>
2019-12-04 17:17:47 +0000

    made a html file with thymeleaf

--- src/main/java/org/spe/biologicaldata/webapplication/HelloController.java
+++ src/main/java/org/spe/biologicaldata/webapplication/HelloController.java
@@ -1,31 +1,25 @@
 package org.spe.biologicaldata.webapplication;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class HelloController {
 
-    /*@RequestMapping("/")
-    public String homePage() {
-        return "HomePage!";
-    }*/
-
-    @RequestMapping("/signin")
-    public String signInPage() {
-        return "Sign in!";
+    @RequestMapping("/")
+    public String index(@RequestParam(name="number", required=false, defaultValue="5") String number, Model model) {
+        List<Integer> numbers = new ArrayList<>();
+        for(int n = 0; n < Integer.parseInt(number); n++) {
+            numbers.add(n+1);
+        }
+        model.addAttribute("numbers", numbers);
+        model.addAttribute("numberOfRows", number);
+        return "index";
     }
-
-    @RequestMapping("/signup")
-    public String signUpPage() {
-        return "Sign up!";
-    }
-
-    @RequestMapping("/html")
-    public String html() {
-        return "test";
-    }
-    
 
 }
