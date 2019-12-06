cf0f1844d9d93b4303b2b5e1cbe8acea9c5ef55e
Kuba <kuba.pt18419@bristol.ac.uk>
2019-12-06 13:58:28 +0000

    some changes to templates and control

--- src/main/java/org/spe/biologicaldata/webapplication/HelloController.java
+++ src/main/java/org/spe/biologicaldata/webapplication/HelloController.java
@@ -3,23 +3,39 @@ package org.spe.biologicaldata.webapplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index(@RequestParam(name="number", required=false, defaultValue="5") String number, Model model) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public  String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String table(HttpServletRequest request, Model model) {
+
+        String number = request.getParameter("number");
+
+        if (number == null || number == "") {
+            number = "0";
+        }
+
         List<Integer> numbers = new ArrayList<>();
         for(int n = 0; n < Integer.parseInt(number); n++) {
             numbers.add(n+1);
         }
+
+
         model.addAttribute("numbers", numbers);
         model.addAttribute("numberOfRows", number);
-        return "index";
+        return "table";
     }
 
 }
