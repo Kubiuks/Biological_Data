cfaa041e30e68e5de2ef6c959502e90502969d8d
Ethan <sk18181@bristol.ac.uk>
2020-02-13 01:01:27 +0000

    added test for text extraction

---
+++ src/main/java/org/spe/biologicaldata/webapplication/Textextract.java
@@ -0,0 +1,21 @@
+
+package org.spe.biologicaldata.webapplication;
+// Imports the Google Cloud client library
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Feature.Type;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Textextract {
+  public static void main(String... args) throws Exception {
+    }}
