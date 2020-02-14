e09279534833955aa842f8625ae1ee4376243fe3
Ethan <sk18181@bristol.ac.uk>
2020-02-14 12:50:56 +0000

    added a gettext method for text extraction

--- src/main/java/org/spe/biologicaldata/webapplication/Textextract.java
+++ src/main/java/org/spe/biologicaldata/webapplication/Textextract.java
@@ -17,5 +17,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Textextract {
-  public static void main(String... args) throws Exception {
-    }}
+
+
+  void getText(String filePath) throws Exception, IOException {
+		Credentials myCredentials = ServiceAccountCredentials.fromStream(
+			new FileInputStream("src/main/resources/textract-15059e3faf5f.json"));
+		
+		ImageAnnotatorSettings imageAnnotatorSettings =
+			ImageAnnotatorSettings.newBuilder()
+			.setCredentialsProvider(FixedCredentialsProvider.create(myCredentials))
+			.build();
+
+
+
+
+    PrintStream out=System.out;
+    
+		//String filePath="src/main/resources/static/images/test.jpg";
+  List<AnnotateImageRequest> requests = new ArrayList<>();
+
+  ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+  Image img = Image.newBuilder().setContent(imgBytes).build();
+  Feature feat = Feature.newBuilder().setType(Type.TEXT_DETECTION).build();
+  AnnotateImageRequest request =
+      AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+  requests.add(request);
+
+  try (ImageAnnotatorClient client = ImageAnnotatorClient.create(imageAnnotatorSettings)) {
+    BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+    List<AnnotateImageResponse> responses = response.getResponsesList();
+
+    for (AnnotateImageResponse res : responses) {
+      if (res.hasError()) {
+        out.printf("Error: %s\n", res.getError().getMessage());
+        return;
+      }
+      for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+		out.printf("Text: %s\n", annotation.getDescription());		
+        //out.printf("Position : %s\n", annotation.getBoundingPoly());
+      }
+    }
+  }
+}
+
+
+
+}
+
+
+
